bc8e0177cafb529948cefdd195a2e38db3c4cb5b
fly1tkg <fly1tkg@gmail.com>
2014-06-21 15:05:10 +0900

    AlermReceiverを作成

---
+++ app/src/main/java/jp/nagaken/wearapp/AlermReceiver.java
@@ -0,0 +1,16 @@
+package jp.nagaken.wearapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by tkg on 2014/06/21.
+ */
+public class AlermReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
