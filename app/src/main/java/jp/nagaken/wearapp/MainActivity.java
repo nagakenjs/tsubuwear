f356f74e650e8a5ce93a7c9d87cc2408c31be4ea
Kenji Nagase <androxion@gmail.com>
2014-06-21 15:43:49 +0900

    Notificationに選択メニューを追加

--- app/src/main/java/jp/nagaken/wearapp/MainActivity.java
+++ app/src/main/java/jp/nagaken/wearapp/MainActivity.java
@@ -2,13 +2,18 @@
 package jp.nagaken.wearapp;
 
 import android.app.Activity;
+import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.preview.support.v4.app.NotificationManagerCompat;
+import android.preview.support.wearable.notifications.RemoteInput;
+import android.preview.support.wearable.notifications.WearableNotifications;
 import android.support.v4.app.NotificationCompat;
-import android.util.Log;
+import android.text.TextUtils;
 import android.widget.Button;
+import android.widget.Toast;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -25,29 +30,46 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.inject(this);
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            Bundle extra = intent.getExtras();
+            if (extra != null) {
+                String choice = extra.getString(NotificationUtil.EXTRA_MENU);
+                if (TextUtils.isEmpty(choice)) {
+                    Toast.makeText(this, "reply none", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, choice, Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 
     @OnClick(R.id.button)
     public void clickButton(Button button) {
-        Log.d(TAG, "click");
+        Resources res = getResources();
+        RemoteInput remoteInput = new RemoteInput.Builder(NotificationUtil.EXTRA_MENU)
+                .setLabel(res.getString(R.string.menu_label))
+                .setChoices(res.getStringArray(R.array.menu_choices))
+                .build();
 
         int notificationId = 1;
-        String eventTitle = "Hello Wear";
-        String eventText = "This is a first application.";
-        String extraName = "extraName";
-        String extraString = "Tap AndroidWear";
 
         Intent viewIntent = new Intent(this, MainActivity.class);
-        viewIntent.putExtra(extraName, extraString);
         PendingIntent viewPendingIntent = PendingIntent.getActivity(this, 0, viewIntent, 0);
 
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.ic_event)
-                .setContentTitle(eventTitle)
-                .setContentText(eventText)
+                .setContentTitle(res.getString(R.string.event_title))
+                .setContentText(res.getString(R.string.event_text))
                 .setContentIntent(viewPendingIntent);
+
+        Notification replyNotification = new WearableNotifications.Builder(notificationBuilder)
+                .addRemoteInputForContentIntent(remoteInput)
+                .build();
+
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-        notificationManager.notify(notificationId, notificationBuilder.build());
+        notificationManager.notify(notificationId, replyNotification);
     }
 
 }
---
+++ app/src/main/java/jp/nagaken/wearapp/NotificationUtil.java
@@ -0,0 +1,5 @@
+package jp.nagaken.wearapp;
+
+public class NotificationUtil {
+    public static final String EXTRA_MENU = "jp.nagaken.wearapp.notifications.EXTRA_MENU";
+}
